b055f848cecf3f20469a56b52377226aafc620d4
BDUB <79788719+B-DUB99@users.noreply.github.com>
2023-11-04 08:56:57 -0400

    Created the PseudoCode.py to get started and an overview

---
+++ PseudoCode.py
@@ -0,0 +1,43 @@
+# File for PseudoCode class
+
+# PseudoCode class
+class PseudoCode:
+	def __init__(self, pseudoCode):
+		self.pseudoCode = pseudoCode
+
+	def getPseudoCode(self):
+		return self.pseudoCode
+
+	def setPseudoCode(self, pseudoCode):
+		self.pseudoCode = pseudoCode
+
+
+	def read_in_data(self, data):
+		# read in data from file
+		# return data
+		pass
+
+	def create_NN(self, data):
+		# create neural network from data
+		# return neural network object
+		pass
+
+	def train_NN(self, NN):
+		# train neural network
+		# return trained neural network
+		pass
+
+	def test_NN(self, NN):
+		# test neural network with test data
+		# return test results
+		pass
+
+	def write_results(self, results):
+		# write results to file
+		# return nothing
+		pass
+
+	def main(self):
+		# main function
+		# return nothing
+		pass
